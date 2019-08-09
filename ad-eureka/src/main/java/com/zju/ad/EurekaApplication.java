a31081faeb6722d43c87fa3a47230a81b2f3aa3d
MageMic <MageMic@users.noreply.github.com>
2019-08-10 01:16:57 +0800

    Initial commit

---
+++ ad-eureka/src/main/java/com/zju/ad/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.zju.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: zjumic
+ * @Date: created at 1:11 2019/8/10
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
